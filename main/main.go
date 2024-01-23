73aea5ac259d22e2a6910cbe16b47b5d04aff8b6
Amaury Franssen <amauryfranssen@gmail.com>
2024-01-23 22:17:30 +0100

    feat: main bdd change

--- main/main.go
+++ main/main.go
@@ -14,6 +14,7 @@ func main() {
 	conf := mysql.Config{
 		User:                 "root",
 		Passwd:               "password",
+		Net:                  "tcp",
 		Addr:                 "127.0.0.1:3306",
 		DBName:               "restaurantbdd",
 		Net:                  "tcp",
