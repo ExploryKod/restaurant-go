005da566b53d017d2268761f8957a91fd36462f8
Nassim <113092073+NasssDev@users.noreply.github.com>
2024-01-24 16:50:38 +0100

    clear cloned line

--- main/main.go
+++ main/main.go
@@ -14,7 +14,6 @@ func main() {
 	conf := mysql.Config{
 		User:                 "root",
 		Passwd:               "password",
-		Net:                  "tcp",
 		Addr:                 "127.0.0.1:3306",
 		DBName:               "restaurantbdd",
 		Net:                  "tcp",
