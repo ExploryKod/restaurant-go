e59a718ea73536c626ebd669342bea7033d4ce89
Nassim <113092073+NasssDev@users.noreply.github.com>
2024-02-10 11:38:12 +0100

    refactor product type struct atrtibute

--- entity/ProductType.go
+++ entity/ProductType.go
@@ -1,7 +1,8 @@
 package entity
 
 type ProductType struct {
-	ID   int    `json:"id"`
-	Name string `json:"name"`
-	Icon string `json:"icon"`
+	ID         int        `json:"id" db:"id"`
+	Name       string     `json:"name" db:"name"`
+	Icon       string     `json:"icon" db:"icon"`
+	Restaurant Restaurant `json:"restaurant" db:"restaurant_id"`
 }
