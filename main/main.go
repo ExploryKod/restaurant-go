dcd33d9dc570315a6cf3e1907434bdecfeeb4fd4
Amaury Franssen <amauryfranssen@gmail.com>
2024-09-17 23:40:44 +0200

    supp env

--- main/main.go
+++ main/main.go
@@ -13,16 +13,20 @@ import (
 )
 
 func main() {
-	errdot := godotenv.Load()
-	if errdot != nil {
-		log.Fatal("Error loading .env file:", errdot)
-	}
+	// errdot := godotenv.Load()
+	// if errdot != nil {
+	// 	log.Error("Error loading .env file:", errdot)
+	// }
 
 	conf := mysql.Config{
-		User:                 os.Getenv("BDD_USER"),
-		Passwd:               os.Getenv("BDD_PASSWORD"),
-		Addr:                 os.Getenv("BDD_PORT"),
-		DBName:               os.Getenv("BDD_NAME"),
+		// User:                 os.Getenv("BDD_USER"),
+		// Passwd:               os.Getenv("BDD_PASSWORD"),
+		// Addr:                 os.Getenv("BDD_PORT"),
+		// DBName:               os.Getenv("BDD_NAME"),
+		User:                 "ueill1e8a2djeyha",
+		Passwd:               "qbpkI0vidAOA5qha2Q6X",
+		Addr:                 "bg34o0geswbybq906ljp-mysql.services.clever-cloud.com:3306"
+		DBName:               "bg34o0geswbybq906ljp",
 		Net:                  "tcp",
 		AllowNativePasswords: true,
 		ParseTime:            true,
